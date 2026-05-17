87213487e2f465ef46a1776ed6d33912f67218be
YOGESHVAR REDDY KALLAM <yogeshvarreddykallam@gmail.com>
2026-05-17 16:44:05 -0400

    Add package.php

---
+++ package.php
@@ -0,0 +1,245 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Package-Info</title>
+
+    <!-- swiper css link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css"/>
+    
+    <!-- front awesome cdn link -->
+    <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+    <!-- custom css file link -->
+    <link rel = "stylesheet" href = "css/style.css">
+
+
+</head>
+<body>
+
+<!-- header section starts -->
+<section class = "header">
+    <a href = "home.php" class = "logo">Travel...</a>
+    <nav class = "navbar">
+        <a href = "home.php">Home</a>
+        <a href = "About.php">About</a>
+        <a href = "package.php">Package</a>
+        <a href = "book.php">Book</a>
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+
+</section>
+<div class="heading" style = "background:url(images/heading-bg-2.jpg) no-repeat ">
+    <h1>Packages</h1>
+</div>
+
+<!-- package section starts -->
+<section class = "packages">
+
+<h1 class = "heading-title">Top Destinations</h1>
+<div class = "box-container">
+    <div class = "box">
+        <div class = "image">
+            <img src = "images/pkgimg-1.jpeg" alt="">
+        </div>
+        <div class="content">
+            <h3>Adventure And Tour</h3>
+            <p>Lorem ipsum dolor sit amet,consectetur adipisci elit, 
+            sed eiusmod tempor incidunt ut labore et dolore magna aliqua. 
+            <a href = "book.php" class = "btn">Book Now</a>     
+        </div>
+    </div>
+
+    <div class = "box">
+        <div class = "image">
+            <img src = "images/pkgimg-2.jpeg" alt="">
+        </div>
+        <div class="content">
+            <h3>Adventure And Tour</h3>
+            <p>Lorem ipsum dolor sit amet,consectetur adipisci elit, 
+            sed eiusmod tempor incidunt ut labore et dolore magna aliqua. 
+            <a href = "book.php" class = "btn">Book Now</a>     
+        </div>
+    </div>
+
+    <div class = "box">
+        <div class = "image">
+            <img src = "images/pkgimg-3.jpeg" alt="">
+        </div>
+        <div class="content">
+            <h3>Adventure And Tour</h3>
+            <p>Lorem ipsum dolor sit amet,consectetur adipisci elit, 
+            sed eiusmod tempor incidunt ut labore et dolore magna aliqua. 
+            <a href = "book.php" class = "btn">Book Now</a>     
+        </div>
+    </div>
+
+    <div class = "box">
+        <div class = "image">
+            <img src = "images/pkgimg-4.jpeg" alt="">
+        </div>
+        <div class="content">
+            <h3>Adventure And Tour</h3>
+            <p>Lorem ipsum dolor sit amet,consectetur adipisci elit, 
+            sed eiusmod tempor incidunt ut labore et dolore magna aliqua. 
+            <a href = "book.php" class = "btn">Book Now</a>     
+        </div>
+    </div>
+
+    <div class = "box">
+        <div class = "image">
+            <img src = "images/pkgimg-5.jpeg" alt="">
+        </div>
+        <div class="content">
+            <h3>Adventure And Tour</h3>
+            <p>Lorem ipsum dolor sit amet,consectetur adipisci elit, 
+            sed eiusmod tempor incidunt ut labore et dolore magna aliqua. 
+            <a href = "book.php" class = "btn">Book Now</a>     
+        </div>
+    </div>
+
+    <div class = "box">
+        <div class = "image">
+            <img src = "images/pkgimg-6.jpeg" alt="">
+        </div>
+        <div class="content">
+            <h3>Adventure And Tour</h3>
+            <p>Lorem ipsum dolor sit amet,consectetur adipisci elit, 
+            sed eiusmod tempor incidunt ut labore et dolore magna aliqua. 
+            <a href = "book.php" class = "btn">Book Now</a>     
+        </div>
+    </div>
+    <div class = "box">
+        <div class = "image">
+            <img src = "images/pkgimg-7.jpeg" alt="">
+        </div>
+        <div class="content">
+            <h3>Adventure And Tour</h3>
+            <p>Lorem ipsum dolor sit amet,consectetur adipisci elit, 
+            sed eiusmod tempor incidunt ut labore et dolore magna aliqua. 
+            <a href = "book.php" class = "btn">Book Now</a>     
+        </div>
+    </div>
+
+    <div class = "box">
+        <div class = "image">
+            <img src = "images/pkgimg-8.jpeg" alt="">
+        </div>
+        <div class="content">
+            <h3>Adventure And Tour</h3>
+            <p>Lorem ipsum dolor sit amet,consectetur adipisci elit, 
+            sed eiusmod tempor incidunt ut labore et dolore magna aliqua. 
+            <a href = "book.php" class = "btn">Book Now</a>     
+        </div>
+    </div>
+
+    <div class = "box">
+        <div class = "image">
+            <img src = "images/pkgimg-9.jpeg" alt="">
+        </div>
+        <div class="content">
+            <h3>Adventure And Tour</h3>
+            <p>Lorem ipsum dolor sit amet,consectetur adipisci elit, 
+            sed eiusmod tempor incidunt ut labore et dolore magna aliqua. 
+            <a href = "book.php" class = "btn">Book Now</a>     
+        </div>
+    </div>
+
+    <div class = "box">
+        <div class = "image">
+            <img src = "images/pkgimg-10.jpeg" alt="">
+        </div>
+        <div class="content">
+            <h3>Adventure And Tour</h3>
+            <p>Lorem ipsum dolor sit amet,consectetur adipisci elit, 
+            sed eiusmod tempor incidunt ut labore et dolore magna aliqua. 
+            <a href = "book.php" class = "btn">Book Now</a>     
+        </div>
+    </div>
+
+    <div class = "box">
+        <div class = "image">
+            <img src = "images/pkgimg-11.jpeg" alt="">
+        </div>
+        <div class="content">
+            <h3>Adventure And Tour</h3>
+            <p>Lorem ipsum dolor sit amet,consectetur adipisci elit, 
+            sed eiusmod tempor incidunt ut labore et dolore magna aliqua. 
+            <a href = "book.php" class = "btn">Book Now</a>     
+        </div>
+    </div>
+    <div class = "box">
+        <div class = "image">
+            <img src = "images/pkgimg-12.jpeg" alt="">
+        </div>
+        <div class="content">
+            <h3>Adventure And Tour</h3>
+            <p>Lorem ipsum dolor sit amet,consectetur adipisci elit, 
+            sed eiusmod tempor incidunt ut labore et dolore magna aliqua. 
+            <a href = "book.php" class = "btn">Book Now</a>     
+        </div>
+    </div>
+</div>
+
+<div class = "load-more"><span class = "btn">Load More</span>
+</div>
+
+</section>
+
+
+<!-- package section ends -->
+<!-- footer section start -->
+<section class = "footer">
+    <div class = "box-container">
+        <div class="box">
+        <h3>Quick links</h3>
+        <a href = "Home.php"> <i class="fas fa-angle-right"></i> Home</a>
+        <a href = "About.php"> <i class="fas fa-angle-right"></i>About</a>
+        <a href = "package.php"> <i class="fas fa-angle-right"></i>Package</a>
+        <a href = "book.php"> <i class="fas fa-angle-right"></i>Book</a>
+        </div>
+        <div class="box">
+        <h3>Extra links</h3>
+        <a href = "#"> <i class="fas fa-angle-right"></i> Any Questions?? </a>
+        <a href = "#"> <i class="fas fa-angle-right"></i> About us </a>
+        <a href = "#"> <i class="fas fa-angle-right"></i> Privacy Policy </a>
+        <a href = "#"> <i class="fas fa-angle-right"></i> Terms Of Use </a>
+        </div>
+
+        <div class="box">
+        <h3>Contact Info</h3>
+        <a href = "#"> <i class="fas fa-phone"></i> +91 999-999-9999 </a>
+        <a href = "#"> <i class="fas fa-phone"></i> +91 888-888-8888 </a>
+        <a href = "#"> <i class="fas fa-envelope"></i> kartheekgrpt@gmail.com </a>
+        <a href = "#"> <i class="fas fa-map"></i> Vijayawada-India-521151 </a>
+        </div>
+
+        <div class="box">
+        <h3>Follow us</h3>
+        <a href = "#"> <i class="fab fa-facebook"></i> Facebook </a>
+        <a href = "#"> <i class="fab fa-twitter"></i> Twitter </a>
+        <a href = "#"> <i class="fab fa-instagram"></i> Instagram </a>
+        <a href = "#"> <i class="fab fa-linkedin"></i> LinkedIn </a>
+        </div>
+    
+    </div>
+
+    <div class="credit">Created By <span> Kartheek </span>| All Rights Reserved </span></div>
+</section>
+<!-- footer section end -->
+
+
+
+
+<!-- swiper js link -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+
+<!-- custom js file link -->
+<script src="js/script.js"></script>
+    
+</body>
+</html>
