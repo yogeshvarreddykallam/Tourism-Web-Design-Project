1a52c428d10cc2a02d6025723038e5c3a9f5f097
YOGESHVAR REDDY KALLAM <yogeshvarreddykallam@gmail.com>
2026-05-17 16:45:07 -0400

    Add js/script.js

---
+++ js/script.js
@@ -0,0 +1,34 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+
+}
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+};
+var swiper = new Swiper(".home-slider", {
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+
+let loadMoreBtn = document.querySelector('.packages .load-more .btn');
+let currentItem = 4;
+
+loadMoreBtn.onclick = () => {
+    let boxes = [...document.querySelectorAll('.packages .box-container .box')];
+    for (var i = currentItem; i < currentItem + 4; i++) {
+        boxes[i].style.display = 'inline-block';
+    };
+    currentItem += 4;
+    if (currentItem >= boxes.length) {
+        loadMoreBtn.style.display = 'none';
+    }
+};
